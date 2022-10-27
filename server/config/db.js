6215afb4722cc21c23eadfe91011b7f8a65c5dbf
Maksat Dushanov <maksatdushanov@MacBook-Air-Maksat.local>
2022-10-27 17:04:07 -0400

    test3 for authorization of users and registration

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
-module.exports = 
-{
+module.exports = {
     //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    // "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://maksat:maksat@mongodbservern1.g30coqk.mongodb.net/book_store?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
